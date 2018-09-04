544b9838ccc7ec3f243f77d1f4d65d8323365b1f
Pranav Avva <pranav.avva@gmail.com>
2018-09-03 22:29:44 -0400

    really chai-d it up!

--- test/spec.js
+++ test/spec.js
@@ -27,6 +27,10 @@ describe("Application launch", function () {
     return this.app.start();
   });
 
+  beforeEach(function () {
+    chaiAsPromised.transferPromiseness = this.app.transferPromiseness;
+  });
+
   this.afterAll(function () {
     if (this.app && this.app.isRunning()) {
       return this.app.stop();
@@ -35,8 +39,14 @@ describe("Application launch", function () {
 
   // Test Cases
 
-  it("shows one initial window", function () {
-    return this.app.client.waitUntilWindowLoaded().getWindowCount().should.eventually.equal(1);
+  it("opens one initial window", function () {
+    return this.app.client.waitUntilWindowLoaded()
+      .getWindowCount().should.eventually.have.at.least(1)
+      .browserWindow.isMinimized().should.eventually.be.false
+      .browserWindow.isVisible().should.eventually.be.true
+      .browserWindow.isFocused().should.eventually.be.true
+      .browserWindow.getBounds().should.eventually.have.property('width').and.be.above(0)
+      .browserWindow.getBounds().should.eventually.have.property('height').and.be.above(0);
   });
 
   it("has correct title", function () {
@@ -55,6 +65,10 @@ describe("DOM Structure", function () {
     return this.app.start();
   });
 
+  beforeEach(function () {
+    chaiAsPromised.transferPromiseness = this.app.transferPromiseness;
+  });
+
   this.afterAll(function() {
     if (this.app && this.app.isRunning()) {
       return this.app.stop();
@@ -64,9 +78,7 @@ describe("DOM Structure", function () {
   // Test Cases
 
   it("has two <link>s in <head>", function() {
-    return this.app.client.getHTML("head link").then(function(elements) {
-      assert.equal(elements.length, 2);
-    });
+    return this.app.client.getHTML("head link").should.eventually.have.property("length").and.equal(2);
   });
 
   it("first <link> is stylesheet", function() {
@@ -76,9 +88,13 @@ describe("DOM Structure", function () {
   it("second <link> is stylesheet", function() {
     return this.app.client.getAttribute("head link:nth-child(5)", "rel").should.eventually.equal("stylesheet");
   });
+
+  it("has 7 <select> or <input> in total", function() {
+    return this.app.client.getHTML("form select, form input").should.eventually.have.property("length").and.equal(7);
+  });
 });
 
-describe("Calculator DOM Structure", function() {
+describe("Calculator Logic", function() {
   this.timeout(timeout);
 
   this.beforeAll(function() {
@@ -89,6 +105,10 @@ describe("Calculator DOM Structure", function() {
     return this.app.start();
   });
 
+  beforeEach(function () {
+    chaiAsPromised.transferPromiseness = this.app.transferPromiseness;
+  });
+
   this.afterAll(function() {
     if (this.app && this.app.isRunning()) {
       return this.app.stop();
@@ -96,11 +116,5 @@ describe("Calculator DOM Structure", function() {
   });
 
   // Test Cases
-
-  it("has 7 <select> or <input> in total", function() {
-    return this.app.client.getHTML("form select, form input").then(function (elements) {
-      assert.equal(elements.length, 7);
-    });
-  });
 
 });
